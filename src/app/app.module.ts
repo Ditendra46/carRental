817a683ec15fce17c97af1b7cf30c314dbdc75a9
ditendra46 <ditendrameesala34@gmail.com>
2025-02-15 22:49:47 +0530

    Fix formatting and import order in app.module.ts; ensure proper newlines in package.json and tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductsComponent } from './components/products/products.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule, MatIconButton } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { SignupComponent } from './signup/signup.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { EditCarComponent } from './edit-car/edit-car.component'
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { EditCarComponent } from './edit-car/edit-car.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DeleteConfirmationComponent } from './delete-confirmation/delete-confirmation.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -40,7 +40,6 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { MatTableModule } from '@angular/material/table';
-
 
 @NgModule({
   declarations: [
@@ -59,33 +58,31 @@ import { MatTableModule } from '@angular/material/table';
     CarFormComponent,
     ConfirmationDialogComponent,
     CarListComponent,
-    CarListComponent,
-    CustomerListComponent,
     CustomerListComponent,
     RentFormComponent,
     RentalListComponent
-    
-    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule, FormsModule, HttpClientModule, BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,ReactiveFormsModule,MatIconModule,
+    ReactiveFormsModule,
+    MatIconModule,
     MatOptionModule,
     MatSelectModule,
     MatNativeDateModule,
     MatDatepickerModule,
     MatProgressSpinnerModule,
-    BrowserAnimationsModule,
     MatDialogModule,
     MatSidenavModule,
-    MatCardModule,
     MatCheckboxModule,
     MatAutocompleteModule,
     MatTableModule
